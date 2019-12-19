bb57de50dc7bfb9d4804f766df5bf485a5eb7596
andreasonny83 <andreasonny83@gmail.com>
2019-12-19 15:18:49 +0000

    fix(log): debug mode is never false

--- src/log.ts
+++ src/log.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-const debug: boolean = Boolean(process.env.DEBUG) || false;
+const debug: boolean = Boolean(process.env.DEBUG) && `${process.env.DEBUG}` === 'true';
 
 export const log = (...args: any): void => {
   if (debug) {
